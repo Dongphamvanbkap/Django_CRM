22120b0c3bf24860d91fe8973aec821a7b12458b
Dongphamvan <dongphamvan.bkap@gmail.com>
2020-05-10 09:00:42 +0700

    add permition

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -126,3 +126,12 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'fakeemailforreal113@gmail.com'
+EMAIL_HOST_PASSWORD = 'thanh123!'
